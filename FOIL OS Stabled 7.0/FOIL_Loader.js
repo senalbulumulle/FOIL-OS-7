652ee20f09d0b5115bb344f716f251fbbd5b1d2b
Senal Bulumulle <39766555+senalbulumulle@users.noreply.github.com>
2019-04-06 15:27:30 -0400

    Grouped files into a folder to add new versions in the future
    Added the FOIL OS 7.0  files into a folder to add more versions of FOIL OS Universal Compatible in the future in folders.

---
+++ FOIL OS Stabled 7.0/FOIL_Loader.js
@@ -0,0 +1,50 @@
+/* 
+  _____    ___    ___   _                                     
+ |  ___|  / _ \  |_ _| | |                                    
+ | |_    | | | |  | |  | |                                    
+ |  _|   | |_| |  | |  | |___                                 
+ |_|      \___/  |___| |_____|                                
+                                                              
+  _                           _                       _       
+ | |       ___     __ _    __| |   ___   _ __        (_)  ___ 
+ | |      / _ \   / _` |  / _` |  / _ \ | '__|       | | / __|
+ | |___  | (_) | | (_| | | (_| | |  __/ | |     _    | | \__ \
+ |_____|  \___/   \__,_|  \__,_|  \___| |_|    (_)  _/ | |___/
+                                                   |__/       
+ __   __   __   __   __   __                                  
+ \ \  \ \  \ \  \ \  \ \  \ \                                 
+  \ \  \ \  \ \  \ \  \ \  \ \                                
+  / /  / /  / /  / /  / /  / /                                
+ /_/  /_/  /_/  /_/  /_/  /_/  
+
+ 
+ Senal Bulumulle 
+
+           .
+2018 openFOIL. 
+This Script allows you to change the settings of the Splash Loader screen 
+ShowPage function feature allows you to change how many milliseconds it takes to "load the page or web application".
+It emulates how long it loads a web page as well. Followed by the setTimeout.
+
+The Whole Purpose of this design is to allow minimalistic feel of the FOIL UI Interface when a user launches a FOIL OS Based Web application, a Web Page, and some mobile devices as 
+an app as well. 
+
+Standard: 27 December 2018 12:06 pm
+
+
+
+
+*/
+
+    var myVar;
+
+    function myFunction() {
+      /* Change the ShowPage */ 
+      
+        myVar = setTimeout(showPage, 1000);
+    }
+
+    function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+    }
